05b93db4f7d827a3a5a23951ddddfe1c4f702cb8
Snip-X <sebastien.letailleur@aramisauto.com>
2019-09-30 16:34:52 +0200

    [ADD] Multiple database dumping [FIX] Fixing some index out of range issue

--- MysqlDumper.py
+++ MysqlDumper.py
@@ -10,21 +10,20 @@ class Dumper(Thread):
     def __init__(self,thread,db,table,user,password,host,folder,compress):
         Thread.__init__(self)
         self.subcmd=""
-        if thread <= 1 && compress !=0 :
-            self.subcmd="pigz -9"
+        self.bashCommand = "mysqldump --quick -h "+str(host)+" -u"+str(user)+" -p"+str(password)+" "+str(db)+" "+str(table)
+        if thread <= 1 and compress !=0 :
+            self.subcmd="pigz -9 -p 1"
             self.myoutput = open(str(folder)+"/dmp_"+str(db)+"/"+str(table)+".sql.gz",'w+')
-        elif thread > 1 && compress !=0:
-            self.subcmd="pigz -9 -p "+thread+""
+        elif thread > 1 and compress !=0:
+            self.subcmd="pigz -9 -p "+str(thread)+""
             self.myoutput = open(str(folder)+"/dmp_"+str(db)+"/"+str(table)+".sql.gz",'w+')
         else:
-            self.bashCommand = "mysqldump --quick -h "+str(host)+" -u"+str(user)+" -p"+str(password)+" "+str(db)+" "+str(table)
             self.myoutput = open(str(folder)+"/dmp_"+str(db)+"/"+str(table)+".sql",'w+')
-        #" > "+
 
     def run(self):
         if self.subcmd !="":
-            process_dump = subprocess.run(self.bashCommand.split(), stdout=self.subcmd)
-            process_compress = subprocess.run(self.bashCommand.split(), stdin=process_dump.stdout stdout=self.myoutput)
+            process_dump = subprocess.Popen(self.bashCommand.split(),stderr=subprocess.DEVNULL, stdout=subprocess.PIPE,shell=False)
+            process_compress = subprocess.run(self.subcmd.split(),stderr=subprocess.DEVNULL, stdin=process_dump.stdout, stdout=self.myoutput,shell=False)
 
         else:
             process_dump = subprocess.run(self.bashCommand.split(), stdout=self.myoutput)
@@ -47,15 +46,18 @@ results = parser.parse_args()
 
 
 mydb = mysql.connector.connect(host=results.host,user=results.user,passwd=results.password)
-mycursor = mydb.cursor()
 nb_threads=results.parallel
 compress=results.compress
 path=results.folder
-db_list=results.databases.split(", ")
+db_list=results.databases.split(",")
+print(db_list)
 for db in db_list:
+    myresult= []
+    mycursor = mydb.cursor()
+    print("We are currently working on :"+str(db))
     Command = "mkdir "+str(results.folder)+"/dmp_"+str(db)
     process = subprocess.Popen(Command.split(), stdout=subprocess.PIPE)
-    mycursor.execute("use "+str(results.databases))
+    mycursor.execute("use "+str(db))
     mycursor.execute("show tables")
     myresult = mycursor.fetchall()
     table_list= [x[0] for x in myresult]
@@ -67,18 +69,24 @@ for db in db_list:
         definitive_threads=nb_threads
     rest=table_list_len%definitive_threads
     while len(table_list)>0:
+        to_del=""
         if rest == 0:
             data=[None]*definitive_threads
             for i in range(definitive_threads):
                 data[i]=Dumper(nb_threads,db,table_list[i],results.user,results.password,results.host,results.folder,compress)
                 data[i].start()
-                table_list.remove(table_list[i])
             for i in range(definitive_threads):
                 data[i].join()
+                to_del=int(definitive_threads)
+            del table_list[0:to_del]
         else:
-            data=[None]*rest
+            data=[None]*(int(rest)+1)
             for i in range(rest):
-                data[i]=Dumper(nb_threads,db,table_list[i],results.user,results.password,results.host,results.folder).start()
-                table_list.remove(table_list[i])
-            rest=(table_list_len-i)%definitive_threads
+                data[i]=Dumper(nb_threads,db,table_list[i],results.user,results.password,results.host,results.folder,compress)
+                data[i].start()
+            for i in range(rest):
+                data[i].join()
+                to_del=int(rest)              
+            rest=(table_list_len-to_del)%definitive_threads
+            del table_list[0:to_del]
 
